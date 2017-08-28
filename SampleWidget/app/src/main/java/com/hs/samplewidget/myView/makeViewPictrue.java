9ba96ad404f97cd69461a59176fbd00c9a52537c
3108003368 <3108003368@qq.com>
2017-08-28 15:23:04 +0800

    add

--- SampleWidget/app/src/main/java/com/hs/samplewidget/myView/makeViewPictrue.java
+++ SampleWidget/app/src/main/java/com/hs/samplewidget/myView/makeViewPictrue.java
@@ -1,10 +1,103 @@
 package com.hs.samplewidget.myView;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+
+import com.hs.samplewidget.R;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
 /**
  * 用于将View保存为图片的工具类
  * 作者：zhanghaitao on 2017/8/28 14:29
  * 邮箱：820159571@qq.com
  */
 
-public class makeViewPictrue {
+public class makeViewPictrue extends AppCompatActivity implements View.OnClickListener {
+
+    private RelativeLayout myView;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.make_view_pictrue);
+        myView = (RelativeLayout) findViewById(R.id.mypic_bitmap);
+        button = (Button) findViewById(R.id.save_pic);
+        button.setOnClickListener(this);
+        //定义一个变量 记录当前权限的状态
+        int checkSlfePermission =
+                ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        //当前么有相应的权限
+        if (checkSlfePermission == PackageManager.PERMISSION_DENIED) {
+            //申请权限 （弹出一个申请权限的对话框）
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+
+        } else
+            //申请到了权限
+            if (checkSlfePermission == PackageManager.PERMISSION_GRANTED) {
+
+            }
+
+    }
+
+    /**
+     * 将bitmap保存为JPG格式的图片
+     *
+     * @param bitmap
+     */
+    private void saveMypic(Bitmap bitmap) {
+        //非空判断
+        if (bitmap == null) {
+            return;
+        }
+        // 保存图片
+        try {
+            File file = new File(Environment.getExternalStorageDirectory(),
+                    SystemClock.currentThreadTimeMillis() + ".png");
+            String url = file.getAbsolutePath();
+            FileOutputStream stream = new FileOutputStream(file);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+            stream.close();
+            Toast.makeText(this, "保存成功，已经保存到" + url, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "保存失败", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public Bitmap makeView2Bitmap(View view) {
+        //View是你需要绘画的View
+        int width = view.getWidth();
+        int height = view.getHeight();
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        //如果不设置canvas画布为白色，则生成透明
+        canvas.drawColor(Color.WHITE);
+        view.draw(canvas);
+        return bitmap;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Bitmap viewBitmap = makeView2Bitmap(myView);
+        saveMypic(viewBitmap);
+    }
+
+
 }
